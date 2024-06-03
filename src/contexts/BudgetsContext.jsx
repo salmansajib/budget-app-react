0c0f763d1fc3f0a4440ba4a4ddc5b7c74ec0aeac
Salman-sajib <sfsajib@gmail.com>
2024-06-03 13:29:02 +0600

    uncategorized budget card

---
+++ src/contexts/BudgetsContext.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+import { createContext, useContext } from 'react';
+import { nanoid } from 'nanoid';
+import useLocalStorage from '../hooks/useLocalStorage';
+
+const BudgetsContext = createContext();
+
+export const UNCATEGORIZED_BUDGET_ID = 'Uncategorized';
+
+export function useBudgets() {
+  return useContext(BudgetsContext);
+}
+
+export const BudgetProvider = ({ children }) => {
+  const [budgets, setBudgets] = useLocalStorage('budgets', []);
+  const [expenses, setExpenses] = useLocalStorage('expenses', []);
+
+  function getBudgetExpenses(budgetId) {
+    return expenses.filter((expense) => expense.budgetId === budgetId);
+  }
+
+  function addBudget({ name, max }) {
+    setBudgets((prevBudgets) => {
+      if (prevBudgets.find((budget) => budget.name === name)) {
+        return prevBudgets;
+      }
+      return [...prevBudgets, { id: nanoid(), name, max }];
+    });
+  }
+
+  function addExpense({ description, amount, budgetId }) {
+    setExpenses((prevExpenses) => {
+      return [...prevExpenses, { id: nanoid(), description, amount, budgetId }];
+    });
+  }
+
+  function deleteBudget({ id }) {
+    // TODO: Deal with expenses
+    setBudgets((prevBudgets) => {
+      return prevBudgets.filter((budget) => budget.id !== id);
+    });
+  }
+
+  function deleteExpense({ id }) {
+    setExpenses((prevExpenses) => {
+      return prevExpenses.filter((expense) => expense.id !== id);
+    });
+  }
+
+  return (
+    <BudgetsContext.Provider
+      value={{
+        budgets,
+        expenses,
+        getBudgetExpenses,
+        addExpense,
+        addBudget,
+        deleteBudget,
+        deleteExpense,
+      }}
+    >
+      {children}
+    </BudgetsContext.Provider>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BudgetProvider } from './contexts/BudgetsContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BudgetProvider>
+      <App />
+    </BudgetProvider>
   </React.StrictMode>
 );
